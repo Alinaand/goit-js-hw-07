5eadbf88acd333e8af6f28c4e911c227bf2fb927
Alinaand <80483769+Alinaand@users.noreply.github.com>
2022-02-10 23:57:30 +0200

    edit 1 task

---
+++ js/01-gallery.js
@@ -0,0 +1,21 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+function createGalleryItems(items){
+    const markup = galleryItems.map(({})=>
+    {
+    return `
+    <div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="small-image.jpg"
+      data-source="large-image.jpg"
+      alt="Image description"
+    />
+  </a>
+</div>
+    `    
+    })
+}
+console.log(galleryItems);
+console.log(markup[0])
